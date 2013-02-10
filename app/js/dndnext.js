ce59af6bd502b4ad031822c2f092415add8d5e37
Ken Stevens <ken.stevens@sympatico.ca>
2013-02-10 01:01:06 -0500

    step working

--- app/js/dndnext.js
+++ app/js/dndnext.js
@@ -1,6 +1,19 @@
 'use strict';
 
 
+
 function CharCtrl($scope) {
+	var stateDesc = {
+		0 : "Determine Ability Scores",
+		1 : "Choose a Race",
+		2 : "Choose a Subrace",
+		3 : "Choose a Class",
+		4 : "Choose a Subclass",
+	}
 	
+	$scope.step=0;
+	
+	$scope.stepDesc = function() {
+		return stateDesc[$scope.step];
+	} 
 }
